6cfd52804d973fb8ba4508c09f8a892598ef8042
drkostas <georgiou.kostas94@gmail.com>
2020-04-23 02:47:58 +0300

    Added unittests for DropboxCloudstore

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,33 +1,40 @@
 import unittest
 from jsonschema.exceptions import ValidationError
 import logging
+import os
 
 from configuration.configuration import Configuration
 
 
 class TestConfiguration(unittest.TestCase):
+    test_data_path: str = os.path.join('test_data', 'test_configuration')
+
     logger = logging.getLogger('TestConfiguration')
 
     def test_schema_validation(self):
         try:
             self.logger.info('Loading the correct Configuration..')
-            configuration = Configuration(config_src='test_data/test_configuration/minimal_conf_correct.yml',
-                                          config_schema_path='../../tests/test_data/test_configuration/minimal_yml_schema.json')
-        except Exception as e:
-            self.logger.error('Error validating the correct yml: &s', e)
+            Configuration(config_src=os.path.join(self.test_data_path, 'minimal_conf_correct.yml'),
+                          config_schema_path=os.path.join('..', '..', 'tests', self.test_data_path,
+                                                          'minimal_yml_schema.json'))
+        except ValidationError as e:
+            self.logger.error('Error validating the correct yml: %s', e)
+            self.fail('Error validating the correct yml')
         else:
             self.logger.info('First yml validated successfully.')
 
         with self.assertRaises(ValidationError):
             self.logger.info('Loading the wrong Configuration..')
-            configuration = Configuration(config_src='test_data/test_configuration/minimal_conf_wrong.yml',
-                                          config_schema_path='../../tests/test_data/test_configuration/minimal_yml_schema.json')
-            self.logger.info('Second yml failed to validate successfully.')
+            Configuration(config_src=os.path.join(self.test_data_path, 'minimal_conf_wrong.yml'),
+                          config_schema_path=os.path.join('..', '..', 'tests', self.test_data_path,
+                                                          'minimal_yml_schema.json'))
+        self.logger.info('Second yml failed to validate successfully.')
 
     def test_to_json(self):
         self.logger.info('Loading Configuration..')
-        configuration = Configuration(config_src='test_data/test_configuration/template_conf.yml',
-                                      config_schema_path='../../tests/test_data/test_configuration/yml_schema.json')
+        configuration = Configuration(config_src=os.path.join(self.test_data_path, 'template_conf.yml'),
+                                      config_schema_path=os.path.join('..', '..', 'tests', self.test_data_path,
+                                                                      'yml_schema.json'))
         expected_json = {'tag': 'production', 'datatore': {
             'config': {'hostname': 'host123', 'username': 'user1', 'password': 'pass2', 'db_name': 'db3', 'port': 3306},
             'type': 'mysql'}, 'cloudstore': {'config': {'api_key': 'apiqwerty'}, 'type': 'dropbox'}}
@@ -35,10 +42,12 @@ class TestConfiguration(unittest.TestCase):
         self.logger.info('Comparing the results..')
         self.assertDictEqual(expected_json, configuration.to_json())
 
+
     def test_to_yaml(self):
         self.logger.info('Loading Configuration..')
-        configuration = Configuration(config_src='test_data/test_configuration/template_conf.yml',
-                                      config_schema_path='../../tests/test_data/test_configuration/yml_schema.json')
+        configuration = Configuration(config_src=os.path.join(self.test_data_path, 'template_conf.yml'),
+                                      config_schema_path=os.path.join('..', '..', 'tests', self.test_data_path,
+                                                                      'yml_schema.json'))
         # Modify and export yml
         self.logger.info('Changed the host and the api_key..')
         configuration.datastore['config']['hostname'] = 'changedhost'
@@ -47,14 +56,18 @@ class TestConfiguration(unittest.TestCase):
         configuration.to_yaml('test_data/test_configuration/actual_output_to_yaml.yml', include_tag=True)
         # Load the modified yml
         self.logger.info('Loading the exported yaml..')
-        modified_configuration = Configuration(config_src='test_data/test_configuration/actual_output_to_yaml.yml',
-                                      config_schema_path='../../tests/test_data/test_configuration/yml_schema.json')
+        modified_configuration = Configuration(
+            config_src=os.path.join(self.test_data_path, 'actual_output_to_yaml.yml'),
+            config_schema_path=os.path.join('..', '..', 'tests', self.test_data_path,
+                                            'yml_schema.json'))
         # Compare
         self.logger.info('Comparing the results..')
         expected_json = {'tag': 'production', 'datatore': {
-            'config': {'hostname': 'changedhost', 'username': 'user1', 'password': 'pass2', 'db_name': 'db3', 'port': 3306},
+            'config': {'hostname': 'changedhost', 'username': 'user1', 'password': 'pass2', 'db_name': 'db3',
+                       'port': 3306},
             'type': 'mysql'}, 'cloudstore': {'config': {'api_key': 'changed_api'}, 'type': 'dropbox'}}
         self.assertDictEqual(expected_json, modified_configuration.to_json())
+
 
     @staticmethod
     def _setup_log(debug: bool = False) -> None:
@@ -66,18 +79,21 @@ class TestConfiguration(unittest.TestCase):
                                       ]
                             )
 
-    @classmethod
-    def setUp(self) -> None:
-        pass
 
     @classmethod
-    def tearDown(self) -> None:
+    def setUp(cls) -> None:
         pass
 
-    # noinspection PyArgumentList
+
+    @classmethod
+    def tearDown(cls) -> None:
+        pass
+
+
     @classmethod
     def setUpClass(cls):
         cls._setup_log()
+
 
     @classmethod
     def tearDownClass(cls):
